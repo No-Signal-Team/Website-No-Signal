ca9e8d5eb8b16078ce02e0c240864edae86e2ccb
anaelle <aromanzin@gaming.tech>
2023-05-17 17:52:58 +0200

    Initial Commit

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const dbo = require("./db/db");
+const express = require("express");
+const app = express();
+const port = 4444;
+
+dbo.connectToServer();
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(port, function () {
+  console.log(`App listening on port ${port}!`);
+});
